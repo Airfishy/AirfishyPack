abef4c86f268b00ab9f7ba982c82ef650dc1aefd
Airfishy <sergio1104carnevale@gmail.com>
2021-05-14 14:27:49 -0400

    a

---
+++ doubleclick.bat
@@ -0,0 +1,38 @@
+@ECHO OFF
+
+:choice
+set /P c=Do you want to download/update Airfishypack?[Y/N]?
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+if /I "%c%" EQU "PENIS" goto :fuck
+goto :choice
+
+
+:somewhere
+
+echo "Installing Airfishy's Pack"
+rmdir "%appdata%\.minecraft\resourcepacks\Air Fishy"
+del "%appdata%\.minecraft\resourcepacks\pack.zip"
+del "%appdata%\.minecraft\resourcepacks\pvppack.zip"
+mkdir "%appdata%\.minecraft\resourcepacks\Airfishy"
+move "pack.png" "%appdata%\.minecraft\resourcepacks\Airfishy"
+move "pack.mcmeta" "%appdata%\.minecraft\resourcepacks\Airfishy"
+move "assets" "%appdata%\.minecraft\resourcepacks\Airfishy"
+echo "Success!"
+pause
+exit
+
+
+:somewhere_else
+
+echo "Canceled"
+pause
+exit
+
+
+:fuck
+
+echo "fuck you"
+pause
+exit
+
